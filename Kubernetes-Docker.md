168726536959889270fedff68daaae3164d352fd
midoks <midoks@163.com>
2018-03-01 12:28:59 +0800

    update

--- Kubernetes-Docker.md
+++ Kubernetes-Docker.md
@@ -12,6 +12,20 @@ swapoff -a
 /lib/systemd/system/
 ```
 
+### etcd 安装
+```
+yum install -y etcd
+
+vi /etc/default/etcd
+
+ETCD_DATA_DIR="/var/lib/etcd/default"
+ETCD_LISTEN_CLIENT_URLS="http://0.0.0.0:2379"
+ETCD_ADVERTISE_CLIENT_URLS="http://0.0.0.0:2379"
+
+
+service etcd restart
+```
+
 ### kube-master
 
 ```
---
+++ kube-master/controller-manager.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+MASTER_ADDRESS=${1:-"8.8.8.18"}
+
+cat <<EOF >/opt/kubernetes/cfg/kube-controller-manager
+KUBE_LOGTOSTDERR="--logtostderr=true"
+KUBE_LOG_LEVEL="--v=4"
+KUBE_MASTER="--master=${MASTER_ADDRESS}:8080"
+
+
+# 在执行主循环之前,先选举一个leader.高可用性运行组件时启用此功能,默认true
+KUBE_LEADER_ELECT="--leader-elect"
+EOF
+
+KUBE_CONTROLLER_MANAGER_OPTS="  \${KUBE_LOGTOSTDERR} \\
+                                \${KUBE_LOG_LEVEL}   \\
+                                \${KUBE_MASTER}      \\
+                                \${KUBE_CONTROLLER_MANAGER_ROOT_CA_FILE} \\
+                                \${KUBE_CONTROLLER_MANAGER_SERVICE_ACCOUNT_PRIVATE_KEY_FILE}\\
+                                \${KUBE_LEADER_ELECT}"
+
+cat <<EOF >/usr/lib/systemd/system/kube-controller-manager.service
+[Unit]
+Description=Kubernetes Controller Manager
+Documentation=https://github.com/kubernetes/kubernetes
+
+[Service]
+EnvironmentFile=-/opt/kubernetes/cfg/kube-controller-manager
+ExecStart=/usr/bin/kube-controller-manager ${KUBE_CONTROLLER_MANAGER_OPTS}
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable kube-controller-manager
+systemctl restart kube-controller-manager
